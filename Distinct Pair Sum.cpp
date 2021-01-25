ff2b84a3c81bd6780bf6b768b240e2984679161a
Archishman Sengupta <71402528+ArchishmanSengupta@users.noreply.github.com>
2021-01-25 20:02:36 +0530

    Created Distinct Pair Sum

---
+++ Distinct Pair Sum.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		long long l,r,result;
+		cin>>l>>r;
+		result=2*(r-l)+1;
+		cout<<result<<endl;
+	}
+	return 0;
+}
