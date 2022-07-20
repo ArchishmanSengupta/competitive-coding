1a5e87108f86f67c2e34c1d22042f7e29730096d
Archishman Sengupta <archishman1808@gmail.com>
2022-07-20 23:25:36 +0530

    prims algorithm

---
+++ 0-CollegePratical/PrimsAlgorithm.c
@@ -0,0 +1,98 @@
+
+/*--------------------AUTHOR: you_know_who, Competitive Programming template for "C"----------------*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#define MOD 1000000007
+#define var -1000000000000
+#define MOD1 998244353
+//#define INF 2e18
+#define sun(i,n) for(ll i=0;i<n;i++)
+#define fo1(i,a,n) for(ll i=a;i<=n;i++)
+#define fo3(val,x) for(auto val : x)
+#define nline "\n"
+#define PI 3.141592653589793238462
+#ifndef ONLINE_JUDGE  // Set
+#define debarr(a,n) cerr<<#a<<":";for(int i=0;i<n;i++)cerr<<a[i]<<" ";cerr<<endl;
+#define debmat(mat,row,col) cerr<<#mat<<":\n";for(int i=0;i<row;i++){for(int j=0;j<col;j++)cerr<<mat[i][j]<<" ";cerr<<endl;}
+
+#define MAX 100001
+#define debug(x) cerr << #x <<" ";_print(x);cerr <<endl;
+#else
+#define debug(x)
+#endif
+typedef double dou;
+typedef long long ll;
+typedef long long int intt;
+typedef unsigned long long ull;
+typedef long double lldou;
+ll globalVariable = 1e18;
+
+#define INF 9999999
+
+#define V 5
+
+
+//----------------------------------------Code Here------------------------------------------
+
+//----------------------------PRIM'S ALGORITHM ----------------
+int G[V][V] = {
+	{0, 9, 75, 0, 0},
+	{9, 0, 95, 19, 42},
+	{75, 95, 0, 51, 66},
+	{0, 19, 51, 0, 31},
+	{0, 42, 66, 31, 0}
+};
+void solve() {
+
+	int no_edge;
+	int selected[V];
+    
+	memset(selected, false, sizeof(selected));
+
+	no_edge = 0;
+	selected[0] = true;
+
+	int x;
+	int y;
+	printf("Edge : Weight\n");
+
+	while (no_edge < V - 1) {
+		int min = INF;
+		x = 0;
+		y = 0;
+
+		for (int i = 0; i < V; i++) {
+			if (selected[i]) {
+				for (int j = 0; j < V; j++) {
+					if (!selected[j] && G[i][j]) {
+						if (min > G[i][j]) {
+							min = G[i][j];
+							x = i;
+							y = j;
+						}
+					}
+				}
+			}
+		}
+		printf("%d - %d : %d\n", x, y, G[x][y]);
+		selected[y] = true;
+		no_edge++;
+	}
+}
+
+//----------------------------------------Driver Function-----------------------------------
+int main() {
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("outpu1.txt", "w", stdout);
+	freopen("error.txt", "w", stderr);
+#endif
+	ll test = 1;
+	for (int i = 0; i < test; i++) {
+		solve();
+	}
+	return 0;
+}
